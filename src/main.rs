513c6591e3c1e223e2a6c2d5a8c3013d347a8604
Erick Alejandro Primelles Perez <eprimelles@datamart.co>
2024-07-10 14:58:09 -0400

    Creating rand logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io;
+use rand::Rng;
 fn main() {
     println!("Welcome to the 2024 World Guessing Cup!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Input your Guess");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You've guessed {}", guess);
+    println!("The secret Number is {secret_number}")
 }
