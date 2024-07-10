7b18a2869d638536ce43e5c7af9e47de7dacc3fc
Erick Alejandro Primelles Perez <eprimelles@datamart.co>
2024-07-10 15:07:28 -0400

    Using match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Welcome to the 2024 World Guessing Cup!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -9,5 +10,10 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You've guessed {}", guess);
-    println!("The secret Number is {secret_number}")
+    println!("The secret Number is {secret_number}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("You Win!")
+    }
 }
