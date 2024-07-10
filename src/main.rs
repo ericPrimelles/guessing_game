8fbfa24126bfbe47f4b0e8c7b687098f8e007dba
Erick Alejandro Primelles Perez <eprimelles@datamart.co>
2024-07-10 15:09:02 -0400

    Parsing string as number

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess_int : i32 = guess.trim().parse().expect("Invalid Number");
     println!("You've guessed {}", guess);
     println!("The secret Number is {secret_number}");
-    match guess.cmp(&secret_number) {
+    match guess_int.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too high!"),
         Ordering::Equal => println!("You Win!")
